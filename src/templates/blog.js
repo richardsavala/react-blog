c1bc1fbfa49bcc1e1e4f81ada7df9e94dfbf6e6c
Richard <82630326+richardsavala@users.noreply.github.com>
2021-11-15 23:03:32 -0700

    gatbsy

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data; // data.markdownRemark holds your post data
+export default function Template({ data }) {
+  const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
   return (
     <div className="blog-post-container">
